a00158cf30c4f42d7dcca946c836287004d55d95
Yusuf Birader <yusuf.birader@hotmail.com>
2023-09-02 14:51:05 +0100

    add main walking skeleton

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	"github.com/pzip"
+)
+
+func main() {
+	archivePath := os.Args[1]
+
+	cli := pzip.CLI{ArchivePath: archivePath, Files: os.Args[2:]}
+	err := cli.Archive()
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
